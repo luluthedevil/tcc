0cc0cfc4b3beff00b1f2ad685f8010f14f43f7ac
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-08-03 13:11:39 -0300

    Auto generated tables

--- backend/kitab/src/app.module.ts
+++ backend/kitab/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
+import { LibraryModule } from './library/library.module';
+import { ChallengesModule } from './challenges/challenges.module';
+import { ProgressModule } from './progress/progress.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, BooksModule, LibraryModule, ChallengesModule, ProgressModule],
   controllers: [AppController],
   providers: [AppService],
 })
